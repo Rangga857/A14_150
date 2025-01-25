23b1fa2db167a251710ec2866b8c135a8d78135f
Rangga857 <azharrangga28@gmail.com>
2025-01-26 06:35:03 +0700

    fixed code tampilan insert Review

--- app/src/main/java/com/example/finalproject/ui/view/review/InsertReviewView.kt
+++ app/src/main/java/com/example/finalproject/ui/view/review/InsertReviewView.kt
@@ -8,6 +8,7 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -31,8 +32,10 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.finalproject.ui.costumwidget.DynamicSelectedTextField
 import com.example.finalproject.ui.viewmodel.reviewviewmodel.FormErrorStateReview
@@ -69,11 +72,35 @@ fun InsertReviewView(
         modifier = modifier,
         snackbarHost = { SnackbarHost(hostState = snackbarHostState) },
     ) { padding ->
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .offset(y = (30).dp)
+                .clip(RoundedCornerShape(topStart = 60.dp, topEnd = 60.dp))
+                .background(Color(0xFF003153)),
+            contentAlignment = Alignment.BottomCenter
+        ) {
+            Text(
+                text = "Insert Reservasi",
+                fontWeight = FontWeight.Bold,
+                fontSize = 25.sp,
+                color = Color.White,
+                modifier = Modifier.padding(bottom = 700.dp)
+            )
+            Text(
+                text = "Silahkan masukkan data dengan sesuai dan tekan tombol simpan!",
+                fontWeight = FontWeight.Medium,
+                fontSize = 12.sp,
+                color = Color.White,
+                modifier = Modifier.padding(bottom = 675.dp)
+            )
+        }
         Column(
             modifier = Modifier
                 .fillMaxSize()
                 .padding(padding)
-                .padding(16.dp),
+                .padding(16.dp)
+                .padding(bottom = 100.dp),
             verticalArrangement = Arrangement.Center,
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
@@ -90,12 +117,6 @@ fun InsertReviewView(
                     verticalArrangement = Arrangement.spacedBy(12.dp),
                     horizontalAlignment = Alignment.CenterHorizontally
                 ) {
-                    Text(
-                        text = "Insert Review",
-                        style = MaterialTheme.typography.headlineSmall,
-                        color = Color(0xFF003153),
-                        modifier = Modifier.padding(bottom = 16.dp)
-                    )
                     InsertBodyReview(
                         reviewuiState = reviewuiState,
                         onValueChange = { updateEvent ->
