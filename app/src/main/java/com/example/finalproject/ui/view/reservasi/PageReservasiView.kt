7b45722af10697d7832281421e7014c3effe39cc
Rangga857 <azharrangga28@gmail.com>
2025-01-26 06:04:01 +0700

    fixed code tampilan page reservasi

--- app/src/main/java/com/example/finalproject/ui/view/reservasi/PageReservasiView.kt
+++ app/src/main/java/com/example/finalproject/ui/view/reservasi/PageReservasiView.kt
@@ -1,6 +1,7 @@
 package com.example.finalproject.ui.view.reservasi
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
@@ -9,10 +10,12 @@ import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.Card
@@ -30,11 +33,14 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.finalproject.R
 import com.example.finalproject.model.Reservasi
@@ -58,25 +64,30 @@ fun PageReservasiScreen(
 
     Scaffold(
         modifier = modifier,
-        topBar = {
-            TopAppBar(
-                title = {
-                    Box(
-                        modifier = Modifier.fillMaxWidth(),
-                        contentAlignment = Alignment.Center
-                    ) {
-                        Text(
-                            text = "Daftar Reservasi",
-                            style = MaterialTheme.typography.titleLarge.copy(color = Color.White)
-                        )
-                    }
-                },
-                colors = TopAppBarDefaults.smallTopAppBarColors(
-                    containerColor = Color(0xFF003153)
-                )
+    ) { innerPadding ->
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .offset(y = (-632).dp)
+                .clip(RoundedCornerShape(bottomEnd = 60.dp, bottomStart = 60.dp))
+                .background(Color(0xFF003153)),
+            contentAlignment = Alignment.BottomCenter
+        ) {
+            Text(
+                text = "Daftar Reservasi",
+                fontWeight = FontWeight.Bold,
+                fontSize = 25.sp,
+                color = Color.White,
+                modifier = Modifier.padding(bottom = 120.dp)
+            )
+            Text(
+                text = "Di bawah ini merupakan data data reservasi:",
+                fontWeight = FontWeight.Medium,
+                fontSize = 12.sp,
+                color = Color.White,
+                modifier = Modifier.padding(bottom = 100.dp)
             )
         }
-    ) { innerPadding ->
         PageVillaStatus(
             pageReservasiUiState = viewModel.reservasiUiState,
             modifier = Modifier.padding(innerPadding),
@@ -172,7 +183,8 @@ fun ReservasiCard(
     modifier: Modifier = Modifier
 ) {
     Card(
-        modifier = modifier,
+        modifier = modifier
+            .padding(top = 70.dp),
         shape = MaterialTheme.shapes.medium,
         elevation = CardDefaults.cardElevation(defaultElevation = 8.dp)
     ) {
